1c6c40f750b7aa97cb20f54038ad021772fbb1b3
jm4853 <jm4853@drexel.edu>
2024-10-25 22:56:52 +0000

    Updated prime_search.c

--- Makefile
+++ Makefile
@@ -5,11 +5,11 @@ PRIME_ROOT_DIR=$(PRIME_VALIDATOR_ROOT_DIR)
 GMP_DIR=$(PRIME_ROOT_DIR)/gmp-6.3.0
 GMP_LIB_DIR=$(GMP_DIR)/.libs
 
-test: algo_tester.c
-	cc algo_tester.c -o algo_test.out
-
 all: prime_search.c
 	cc prime_search.c -o prime_search.out -I$(GMP_DIR) -L$(GMP_LIB_DIR) -lgmp
+
+test: algo_tester.c
+	cc algo_tester.c -o algo_test.out
 
 clean:
 	rm *.out
--- prime_search.c
+++ prime_search.c
@@ -23,58 +23,13 @@ close_file(FILE *fp) {
 }
 
 
-void
-handle_threads(mpz_t num, mpz_t start, len_t len, unsigned int threads) {
-printf("In handle threads\n");
-    pthread_t tids[threads];
-    func_arg_t a;
-    mpz_t t;
-    unsigned int i;
-    unsigned int seg = len / threads;
-
-    mpz_init(t);
-printf("num: ");
-mpz_out_str(stdout, 10, num);
-printf("\n");
-
-    for(i = 0; i < threads; i++) {
-printf("it: %d\n", i);
-        pthread_mutex_lock(&arg_lock);
-        a.l = seg;
-        a.n = (mpz_t *)(&num);
-        a.s = (mpz_t *)(&start);
-printf("l: %d\n", seg);
-printf("n: ");
-mpz_out_str(stdout, 10, (*((mpz_t *)(a.n))));
-printf("\ns: ");
-mpz_out_str(stdout, 10, *(a.s));
-printf("\n");
-        pthread_mutex_unlock(&arg_lock);
-
-        if(pthread_create(&(tids[i]), NULL, test_range, (void *)(&a)) != 0) {
-            printf("Could not create thread\n");
-            return;
-        }
-printf("Thread created\n");
-        pthread_mutex_lock(&arg_lock);
-		mpz_add_ui(t, start, seg);
-		mpz_set(start, t);
-        pthread_mutex_unlock(&arg_lock);
-    }
-printf("Collecting threads\n");
-    for(i = 0; i < threads; i++) {
-        pthread_join(tids[i], NULL);
-    }
-
-}
-
-handle_threads(mpz_t num, mpz_t start, len_t len, unsigned int threads) {
 
 
 int
 test_range(mpz_t num, mpz_t start, len_t len) {
     FILE *fp;
 	mpz_t t, cur1, cur2;
+    len_t i;
 
 printf("len: %ld\n", len);
 
@@ -100,8 +55,7 @@ printf("After init\n");
 			mpz_out_str(fp, 10, cur1);
 			fprintf(fp, " == 0\n");
             close_file(fp);
-            finished = 1;
-			return NULL;
+			return 0;
 		}
 		mpz_mod(t, num, cur2);	// t = num % curl2
 		if (mpz_sgn(t) == 0) {	// if t == 0
@@ -112,8 +66,7 @@ printf("After init\n");
 			mpz_out_str(fp, 10, cur2);
 			fprintf(fp, " == 0\n");
             close_file(fp);
-            finished = 1;
-			return NULL;
+			return 0;
 		}
 		
 mpz_out_str(stdout, 10, cur1);
@@ -133,42 +86,29 @@ printf("\n");
     mpz_out_str(fp, 10, t);
     fprintf(fp, ")\nPrimality not detected\n");
     close_file(fp);
-	return NULL;
+	return 1;
 }
 
 int
 main() {
-	// printf("Passed %d tests\n", test(1000));
 
     
-	// pthread_t tid;
-    // func_arg_t args;
-    // args.l = 100;
-    // printf("args addr: %p\n", &args);
-    // printf("l addr:    %p\n", &(args.l));
-    // printf("Thing going\n");
-    // Starts must be in the form 6n + 5
-    // mpz_init_set_str(args.s, "5", 10);
     mpz_t num;
     mpz_t start;
+    len_t len;
     mpz_init_set_str(num, "12848751825131232133313313131318907819791", 10);
     mpz_init_set_str(start, "101", 10); // 5 + 6*16
+    len = 100;
     // 100489 = 317^2
-printf("Before handle_threads\n");
-    handle_threads(num, start, 100, 1);
-printf("After handle_threads\n");
 
-    // printf("Init-ed\n");
-    // if(pthread_create(&tid, NULL, test_range, (void *)(&args)) != 0) {
-    //     printf("Could not create thread\n");
-    //     return 1;
-    // }
-    // printf("Waiting\n");
-    // pthread_join(tid, NULL);
-    // printf("Thread Exited\n");
+printf("After main init\n");
+    
 
-	
-
+    if(test_range(num, start, len)) {
+        printf("Number prime\n");
+    } else {
+        printf("Number not prime\n");
+    }
 
 
 	return 0;
